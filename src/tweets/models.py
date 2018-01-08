866757a0eb1a63f03d69e5b6daa5f0bcd2b4b30e
Amy Dang <dttmy2020@gmail.com>
2018-01-08 20:54:21 +0700

    Add a simple Tweet model

--- src/tweets/models.py
+++ src/tweets/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Tweet(models.Model):
+	# user
+	content 	= models.CharField(max_length=140)
+	updated	= models.DateTimeField(auto_now=True)
+	timestamp	= models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return str(self.content)
