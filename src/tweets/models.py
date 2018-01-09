516043022ac58ac9aceca40af8fb4856595f4914
Amy Dang <dttmy2020@gmail.com>
2018-01-09 20:47:39 +0700

    Add tweet and user association

--- src/tweets/models.py
+++ src/tweets/models.py
@@ -1,12 +1,20 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.db import models
 
 # Create your models here.
 
 
+def get_sentinel_user():
+	return get_user_model().objects.get_or_create(username='deleted')[0]
+
 class Tweet(models.Model):
-	# user
+	user = models.ForeignKey(
+		settings.AUTH_USER_MODEL,
+		on_delete=models.SET(get_sentinel_user),
+		)
 	content 	= models.CharField(max_length=140)
-	updated	= models.DateTimeField(auto_now=True)
+	updated		= models.DateTimeField(auto_now=True)
 	timestamp	= models.DateTimeField(auto_now_add=True)
 
 	def __str__(self):
